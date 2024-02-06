01fcdb39189deb2ca4f570c5cebcb1992fb6d6ee
bas-ene <enea.basilico@gmail.com>
2024-02-06 15:25:36 +0100

    implemented never have i ever, bettered trivia

--- app.py
+++ app.py
@@ -1,24 +1,54 @@
+import random
 from deck_lib import Deck, Card
 from trivia_lib import Trivia
+from neverhaveiever_lib import NeverHaveIEver as Nhie
 
-def handleCard(card: Card) -> bool:
+def handleCard(card: Card):
 
     if card.value == 'JOKER':
-        return False
+        print('You lostt!')
+        return 
+
     match card.suit:
-        case 'CLUBS' | 'DIAMONDS' | 'HEARTS' | 'SPADES':
+        case 'CLUBS' :
             question = trivia.serve_question()
             print(f'Question: {question["question"]["text"]}')
+            print('Options:')
+            possible_answers = question['incorrectAnswers'] + [question['correctAnswer']]
+            random.shuffle(possible_answers)
+            for i, answer in enumerate(possible_answers):
+                print(f'{i + 1}. {answer}')
+            user_answer_idx = input('Your answer: ')
+            while not user_answer_idx.isdigit() or int(user_answer_idx) < 1 or int(user_answer_idx) > 4:
+                user_answer_idx = input('Your answer: ')
+                print('Invalid answer')
 
-            user_answer = str(input('Your answer: '))
-            success = user_answer.lower() == question['correctAnswer'].lower()
+            user_answer = possible_answers[int(user_answer_idx) - 1]
+            success = user_answer == question['correctAnswer']
 
             if success:
                 print('Correct!')
             else:
+                print('Incorrect!')
                 print(f'Correct answer: {question["correctAnswer"]}')
+            
+        case 'DIAMONDS':
+            print(nhie.serve_nhie())
 
-            return success
+            print('Have you ever done this? (y/n): ')
+            user_answer = input()
+            while user_answer not in ['y', 'n']:
+                user_answer = input('Invalid answer. Please enter y or n: ')
+            if user_answer == 'y':
+                print('You\'ve lcst!')
+            else:
+                print('You\'ve won!')
+
+        case 'HEARTS':
+            print('You won a heart!')
+
+        case 'SPADES':
+            print('You won a spade!')
 
         case _: 
             raise Exception('Invalid suit')
@@ -26,13 +56,10 @@ def handleCard(card: Card) -> bool:
 if __name__ == '__main__':
     deck = Deck()
     trivia = Trivia()
+    nhie = Nhie(r_rated=True)
     while input('Draw a card? (y/n): ') == 'y':
         drawn = deck.draw(1)[0]
         print(f'You drew: {drawn}')
-
-        has_won = handleCard(drawn)
-
-        if has_won:
-            print('You won!')
-        else: 
-            print('You lost!')
+        
+        # handleCard(drawn)
+        handleCard(Card('DIAMONDS', '2'))
